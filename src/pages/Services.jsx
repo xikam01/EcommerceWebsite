b5dfd933a9527e74df4c62aa1ac54f0c26f32296
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-10-30 13:18:05 -0700

    waxaan kusoo daray Service page kii

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,22 +4,94 @@ import style from '../components/services.module.css'
 
 const Services = () => {
   return (
-    <div className={style.container}>
-          <h1>Our Service</h1>
-      <div className={style.boxses}>
-      <div className={style.box}>
-          <h5>iphone</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <img src='/public/samsung.jpg'></img>
-    </div>
-    <div className={style.box}>
-          <h5>iphone</h5>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          <img src='/public/samsung.jpg'></img>
-    </div>
-          </div>
-    
-    </div>
+      <div className={style.container}>
+      <h1>Our Service</h1>
+
+  <div className={style.boxses}>
+  <div className={style.box}>
+      <img src='/public/hp-computer.jpg'></img>
+      <h5>Lapton <span>hp</span></h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+<div className={style.box}>
+      <img src='/public/serv.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+<div className={style.box}>
+      <img src='/public/slide-4.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+<div className={style.box}>
+      <img src='/public/a03core.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+<div className={style.box}>
+      <img src='/public/shops-4.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+  </div>
+
+  <br/>
+  <div className={style.boxses}>
+  <div className={style.box}>
+      <img src='/public/shops-3.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+<div className={style.box}>
+      <img src='/public/flash-2.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+<div className={style.box}>
+      <img src='/public/shops-6.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+<div className={style.box}>
+<img src='/public/shops-10.png'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+<div className={style.box}>
+      <img src='/public/samsung.jpg'></img>
+      <h5>iphone</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <br/><br/>
+      <span>$500</span>
+</div>
+
+  </div>
+
+
+</div>
   )
 }
 
