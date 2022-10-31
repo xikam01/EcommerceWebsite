376a9ae3d7aa245969fd8771c5bfaaa8c7561573
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-10-31 05:38:32 -0700

    waxaan wax ka badalay home and about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,59 +4,67 @@ import style from '../components/about.module.css'
 const About = () => {
   return (
    
-        <div className={style.container}>
-          <h1>About</h1>
-        <div className={style.fle}>
-        <div className={style.cercales}>
-            <div className={style.cer1}></div>
-            <div className={style.cer2}></div>
-            <div className={style.cer3}></div>
-            <div className={style.cer4}></div>
-            <div className={style.cer5}></div>
-            <div className={style.cer6}></div>
-            <div className={style.cer7}></div>
-            <div className={style.cer8}></div>
-            <div className={style.cer9}></div>
-            <div className={style.cer10}></div>
-            <div className={style.cer11}></div>
-            <div className={style.cer12}></div>
-            <div className={style.cer13}></div>
-            <div className={style.cer14}></div>
-            <div className={style.cer15}></div>
-            <div className={style.cer16}></div>
-            <div className={style.cer17}></div>
-            <div className={style.cer18}></div>
-            <div className={style.cer19}></div>
-            <div className={style.cer20}></div>
-          </div>
-          <div className={style.Readingabout}>
-          <div className={style.cer20}></div>
-          <div className={style.cer19}></div>
-            <div className={style.cer18}></div>
-            <div className={style.cer17}></div>
-            <div className={style.cer16}></div>
-            <div className={style.cer15}></div>
-            <div className={style.cer14}></div>
-            <div className={style.cer13}></div>
-            <div className={style.cer12}></div>
-            <div className={style.cer11}></div>
-            <div className={style.cer10}></div>
-            <div className={style.cer9}></div>
-            <div className={style.cer8}></div>
-            <div className={style.cer7}></div>
-            <div className={style.cer6}></div>
-            <div className={style.cer5}></div>
-            <div className={style.cer4}></div>
-            <div className={style.cer3}></div>
-            <div className={style.cer2}></div>
-           <div className={style.cer1}></div>
-          </div>
-        </div>
-       <div className={style.code}>
-       <h2>Suqadara is a invented 2022/10/29/ and invented <span>Team of Tiigsi</span></h2>
-        <h3>Suqadara by Design and code <span>eng mohamed cabdiqabi & eng cabdiraxman</span></h3>
-       </div>
-        </div>
+      //   <div className={style.container}>
+      //     <h1>About</h1>
+      //   {/* <div className={style.fle}>
+      //   <div className={style.cercales}>
+      //       <div className={style.cer1}></div>
+      //       <div className={style.cer2}></div>
+      //       <div className={style.cer3}></div>
+      //       <div className={style.cer4}></div>
+      //       <div className={style.cer5}></div>
+      //       <div className={style.cer6}></div>
+      //       <div className={style.cer7}></div>
+      //       <div className={style.cer8}></div>
+      //       <div className={style.cer9}></div>
+      //       <div className={style.cer10}></div>
+      //       <div className={style.cer11}></div>
+      //       <div className={style.cer12}></div>
+      //       <div className={style.cer13}></div>
+      //       <div className={style.cer14}></div>
+      //       <div className={style.cer15}></div>
+      //       <div className={style.cer16}></div>
+      //       <div className={style.cer17}></div>
+      //       <div className={style.cer18}></div>
+      //       <div className={style.cer19}></div>
+      //       <div className={style.cer20}></div>
+      //     </div>
+      //     <div className={style.Readingabout}>
+      //     <div className={style.cer20}></div>
+      //     <div className={style.cer19}></div>
+      //       <div className={style.cer18}></div>
+      //       <div className={style.cer17}></div>
+      //       <div className={style.cer16}></div>
+      //       <div className={style.cer15}></div>
+      //       <div className={style.cer14}></div>
+      //       <div className={style.cer13}></div>
+      //       <div className={style.cer12}></div>
+      //       <div className={style.cer11}></div>
+      //       <div className={style.cer10}></div>
+      //       <div className={style.cer9}></div>
+      //       <div className={style.cer8}></div>
+      //       <div className={style.cer7}></div>
+      //       <div className={style.cer6}></div>
+      //       <div className={style.cer5}></div>
+      //       <div className={style.cer4}></div>
+      //       <div className={style.cer3}></div>
+      //       <div className={style.cer2}></div>
+      //      <div className={style.cer1}></div>
+      //     </div>
+      //   </div>
+      //  <div className={style.code}> */}
+      //  <h2>Suqadara is a invented 2022/10/29/ and invented <span>Team of Tiigsi</span></h2>
+      //   <h3>Suqadara by Design and code <span>eng mohamed cabdiqabi & eng cabdiraxman</span></h3>
+      //  </div>
+     <div className={style.containe}>
+      <div className={style.sawir}>
+        <img src='/public/unspashabout.jpg'></img>
+      </div>
+      <div className={style.text}>
+        /<h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam ex incidunt possimus libero expedita cum necessitatibus atque voluptates officiis soluta?</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, obcaecati ipsam! Dolores, impedit accusamus cum in minima ducimus molestiae ipsum deserunt pariatur, nobis odit quidem. Quisquam libero recusandae sapiente eum? Illum, rerum numquam dignissimos, molestiae odit, modi eius doloremque quos dolor natus molestias voluptatibus. Doloribus explicabo soluta laboriosam quae qui id dignissimos, rem obcaecati distinctio quaerat, accusantium hic, voluptatum voluptas perspiciatis? Non exercitationem consectetur odit omnis temporibus delectus quaerat consequatur, quos nemo eius nihil quia laudantium at expedita quis esse ad! Itaque suscipit eum perspiciatis maxime facilis! Itaque eligendi aliquam, nam, minima repellat harum fugiat aliquid rerum sunt dolor eos porro. Dolores quod blanditiis unde fugiat nostrum dolore accusamus nam amet quo animi reprehenderit tenetur cumque beatae autem deserunt cupiditate eius non laudantium earum provident, ut vero molestiae. Iusto, sit iste sunt cum exercitationem nostrum. Excepturi dignissimos dolore aut quam.</p>
+      </div>
+     </div>
        
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <div>
       <div className={style.flexes}>
+      <div className={style.text}>
+        <h1>welcome  to Suqadar shoping center</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis architecto reprehenderit est tempore possimus perferendis qui nulla tempora non! Molestias unde est quaerat id assumenda corporis beatae facere! Laudantium beatae animi magnam dolor non! Inventore excepturi facilis delectus fugiat impedit.</p>
+        <button className={style.fce}>Hadda Dalbo</button>
+      </div>
       <div className={style.image}>
         <img src="/public/business.png" alt="" />
-      </div>
-      <div className={style.text}>
-        <h1>Hello the Business man</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ab laborum quasi in assumenda at! Dignissimos voluptatum recusandae aspernatur earum!</p>
-        <button className={style.btn}>Hadda Dalbo</button>
       </div>
 
 
