cf5709d50816e10cded4b7674e3934cdc6e7c18c
xikam01 <cabdiraxmaanxikam190@gmail.com>
2022-10-31 02:55:49 -0700

    added page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import Header from '../components/Header'
+import style from '../components/about.module.css'
 const About = () => {
   return (
-    <div>
-        <h1>Hello World About jsx</h1>
+   
+        <div className={style.container}>
+          <h1>About</h1>
+        <div className={style.fle}>
+        <div className={style.cercales}>
+            <div className={style.cer1}></div>
+            <div className={style.cer2}></div>
+            <div className={style.cer3}></div>
+            <div className={style.cer4}></div>
+            <div className={style.cer5}></div>
+            <div className={style.cer6}></div>
+            <div className={style.cer7}></div>
+            <div className={style.cer8}></div>
+            <div className={style.cer9}></div>
+            <div className={style.cer10}></div>
+            <div className={style.cer11}></div>
+            <div className={style.cer12}></div>
+            <div className={style.cer13}></div>
+            <div className={style.cer14}></div>
+            <div className={style.cer15}></div>
+            <div className={style.cer16}></div>
+            <div className={style.cer17}></div>
+            <div className={style.cer18}></div>
+            <div className={style.cer19}></div>
+            <div className={style.cer20}></div>
+          </div>
+          <div className={style.Readingabout}>
+          <div className={style.cer20}></div>
+          <div className={style.cer19}></div>
+            <div className={style.cer18}></div>
+            <div className={style.cer17}></div>
+            <div className={style.cer16}></div>
+            <div className={style.cer15}></div>
+            <div className={style.cer14}></div>
+            <div className={style.cer13}></div>
+            <div className={style.cer12}></div>
+            <div className={style.cer11}></div>
+            <div className={style.cer10}></div>
+            <div className={style.cer9}></div>
+            <div className={style.cer8}></div>
+            <div className={style.cer7}></div>
+            <div className={style.cer6}></div>
+            <div className={style.cer5}></div>
+            <div className={style.cer4}></div>
+            <div className={style.cer3}></div>
+            <div className={style.cer2}></div>
+           <div className={style.cer1}></div>
+          </div>
         </div>
+       <div className={style.code}>
+       <h2>Suqadara is a invented 2022/10/29/ and invented <span>Team of Tiigsi</span></h2>
+        <h3>Suqadara by Design and code <span>eng mohamed cabdiqabi & eng cabdiraxman</span></h3>
+       </div>
+        </div>
+       
   )
 }
 
